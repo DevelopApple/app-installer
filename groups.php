f8069ac2d770d79378d3ec1e24cd67391ae0d0ef
Techwolf12 <christiaan@uncinc.nl>
2017-07-28 16:15:00 +0200

    group stuff

--- groups.php
+++ groups.php
@@ -3,9 +3,10 @@
 * Groups of multiple applications
 */
 switch($_GET["apps"]) {
-	case "clientwithmultipleapps":
-		$appstolist = array("com.app1","com.app2");
+	case "diasemi":
+		$appstolist = array("com.diasemi.hcs-wearables","com.diasemi.iot-sensor-app","com.dialog.wearables", "com.dialog.wearableshcs");
 		break;
 }
+
 
 ?>
